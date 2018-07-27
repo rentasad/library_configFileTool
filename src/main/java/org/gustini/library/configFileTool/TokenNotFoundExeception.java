a449228bc11e127f1de457941e7b0beb566553e7
Matthias Staud <matthias.staud@gustini.de>
2018-07-27 14:29:26 +0200

    Initial Commit

---
+++ src/main/java/org/gustini/library/configFileTool/TokenNotFoundExeception.java
@@ -0,0 +1,21 @@
+package org.gustini.library.configFileTool;
+
+public class TokenNotFoundExeception extends Exception
+{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/* (non-Javadoc)
+	 * @see java.lang.Throwable#getMessage()
+	 */
+	@Override
+	public String getMessage()
+	{
+		System.err.println("Angefordertes Token der Configuration wurde nicht gefunden.");
+		return super.getMessage();
+	}
+
+}
